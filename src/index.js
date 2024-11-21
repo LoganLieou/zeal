3d52509e3c25207c0adb26f508faf2376f0e27d7
loganlieou <logan.lieou@gmail.com>
2024-11-20 21:54:44 -0600

    gapped

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import m from "mithril"
 
 function Component() {
-	// How do we do state here, how do I do useful javascript along with wowo I just wanted to
+	// How do we do state here, how do I do useful javascript along with wowo I just wanted to farm for a
 	return {
 		view: () => (
 			<main>
