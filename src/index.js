882bbe2ff4f5b4a1566f2c06d4efd9a620218ce7
loganlieou <logan.lieou@gmail.com>
2024-11-20 21:54:21 -0600

    gapped

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import m from "mithril"
 
 function Component() {
-	// How do we do state here, how do I do useful javascript along with this how
+	// How do we do state here, how do I do useful javascript along with this how do
 	return {
 		view: () => (
 			<main>
