34798cdf5b0ed7a7d619dbc5793b8f9e3e7b9905
loganlieou <logan.lieou@gmail.com>
2024-11-20 21:19:40 -0600

    gapped

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function Component() {
 			<main>
 				<h1>Zeal</h1>
 				<p>
-					I will add contents here in the future for d
+					I will add contents here in the future for doing somethi
 				</p>
 			</main>
 		)
